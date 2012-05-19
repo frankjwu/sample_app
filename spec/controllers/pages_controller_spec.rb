7435d1d661095bb25b90ecc7e4dc26a8acc8724c
Frank Wu <fwu2012@gmail.com>
2012-05-19 23:51:37 +0800

    Added user following and feed

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,14 +8,38 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    
+    describe "when not signed in" do
+      
+      before(:each) do
+        get :home
+      end
+      
+      it "should be successful" do
+        response.should be_success
+      end
+    
+      it "should have the right title" do
+        response.should have_selector("title",
+          :content => @base_title + " | Home")
+      end
     end
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-        :content => @base_title + " | Home")
+    
+    describe "when signed in" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+      
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
   end
 
@@ -24,6 +48,7 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
@@ -36,6 +61,7 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
@@ -48,11 +74,11 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+    
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
         :content => @base_title + " | Help")
     end
   end
-
 end
